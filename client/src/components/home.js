0c4b47e367ea7c342aca42df11323b2081a3f57f
boyangmatsapola <boyangbass@gmail.com>
2019-12-12 15:16:20 -0600

    ready to delete these images

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import {text, microphones , preamps, outboard, snakes, digital , amps, instruments} from '../components/text'
+import { text, microphones, preamps, outboard, snakes, digital, amps, instruments } from '../components/text'
+import Mics from '../media/microphones.JPG'
+import Amps from '../media/Amps.JPG'
 import Equipment from './equipment'
 
 
@@ -12,14 +14,45 @@ const Home = (props) => {
                     <h1>PALLET SOUND</h1>
                     <h3>TAKE YOUR MUSIC FURTHER</h3>
                 </div>
-                    <p>{text}</p>
-                    <Equipment {...props} data= {microphones} title={`Microphones`}></Equipment>
-                    <Equipment {...props} data = {preamps} title={`Pre Amplifiers`}></Equipment>
-                    <Equipment {...props} data = {outboard} title={`Outboard`}></Equipment>
-                    <Equipment {...props} data = {snakes} title={`Snakes`}></Equipment>
-                    <Equipment {...props} data = {digital} title={`Digital Plugins`}></Equipment>
-                    <Equipment {...props} data = {amps} title={`Amplifiers & Cabinets:(some only available upon request)`}></Equipment>
-                    <Equipment {...props} data = {instruments} title={`Instruments`}></Equipment>
+                <p>{text}</p>
+                <div className="rowCont">
+                    <img src={Mics} alt='microphones' />
+                    <Equipment  {...props} data={microphones} type={{ a: 'subHeader1', b: 'list1', c : 'listItems1'}} title={`Microphones`}></Equipment>
+                </div>
+                <div className="rowCont">
+                    <Equipment {...props} data={preamps} type={{ a: 'subHeader2', b: 'list2', c : 'listItems2'}} title={`Pre Amplifiers`}></Equipment>
+                    <img src={Amps} alt='microphones' />
+                </div>
+                
+                <div className="rowCont">
+                    <img src={Mics} alt='microphones' />
+                <Equipment {...props} data={outboard} type={{ a: 'subHeader1', b: 'list1', c : 'listItems1'}} title={`Outboard`}></Equipment>
+                    
+                </div>
+                <div className="rowCont">
+                <Equipment {...props} data={snakes} type={{ a: 'subHeader2', b: 'list2', c : 'listItems2'}} title={`Snakes`}></Equipment>
+                    <img src={Amps} alt='microphones' />
+                </div>
+                <div className="rowCont">
+                    <img src={Amps} alt='microphones' />
+                <Equipment {...props} data={snakes} type={{ a: 'subHeader1', b: 'list1', c : 'listItems1'}} title={`Snakes`}></Equipment>
+                </div>
+                <div className="rowCont">
+                <Equipment {...props} data={digital} type={{ a: 'subHeader2', b: 'list2', c : 'listItems2'}} title={`Digital Plugins`}></Equipment>
+                    <img src={Mics} alt='microphones' />
+                    
+                </div>
+                <div className="rowCont">
+                    <img src={Amps} alt='microphones' />
+                <Equipment {...props} data={amps} type={{ a: 'subHeader1', b: 'list1', c : 'listItems1'}} title={`Amplifiers & Cabinets:(some only available upon request)`}></Equipment>
+                </div>
+                <div className="rowCont">
+                <Equipment {...props} data={instruments} type={{ a: 'subHeader2', b: 'list2', c : 'listItems2'}} title={`Instruments`}></Equipment>
+                    <img src={Mics} alt='microphones' />
+                    
+                </div>
+
+
             </div>
 
         </div>
