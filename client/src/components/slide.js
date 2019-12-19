8b3bf64b322ab2827153c35e48a2966c7132e4a4
Michelle Wuebben <wuebben.michelle@yahoo.com>
2019-12-18 20:54:39 -0600

    fixed background squishy and subtle fade

--- client/src/components/slide.js
+++ client/src/components/slide.js
@@ -4,6 +4,9 @@ import Carousel from 'react-bootstrap/Carousel';
 import Console from '../media/console.JPG'
 import RoomA from '../media/RoomA.JPG'
 import AltRoomA from '../media/AltRoomA.JPG'
+
+
+
  const Slide = (props) => {
     return (
         <div className = "slideCont" id="landing">
@@ -13,43 +16,50 @@ import AltRoomA from '../media/AltRoomA.JPG'
                 </div>
         <Carousel>
         <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={AltRoomA}
-            alt="First slide"
-          />
+          
+          <div className="first-slide" >
+            {/* <img
+              className="d-block w-100"
+              src={AltRoomA}
+              alt="First slide"
+            /> */}
+          
           <Carousel.Caption>
             <h3>1048 W 37th Street
             Chicago, Illinois</h3>
             <a className ="next"href= '#Engineers'>Scroll Down</a>
           </Carousel.Caption>
+          </div>
         </Carousel.Item>
         <Carousel.Item>
-          <img
+
+          {/* <img
             className="d-block w-100"
             src={Console}
             alt="Third slide"
-          />
-      
+          /> */}
+        <div className="second-slide">
           <Carousel.Caption>
           <h3>1048 W 37th Street
             Chicago, Illinois</h3>
             <a className ="next"href= '#Engineers'>Scroll Down</a>
           </Carousel.Caption>
+          </div>
         </Carousel.Item>
         <Carousel.Item>
-          <img
+          {/* <img
             className="d-block w-100"
             src={RoomA}
             alt="Third slide"
-          />
-      
+          /> */}
+        <div class="third-slide">
           <Carousel.Caption>
           <h3>1048 W 37th Street
             Chicago, Illinois</h3>
             <a className ="next"href= '#Engineers'>Scroll Down</a>
             
           </Carousel.Caption>
+          </div>
         </Carousel.Item>
       </Carousel>
       </div>
