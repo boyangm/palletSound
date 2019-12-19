8f63515fd19cc21cb30272914f306190825c3809
boyangmatsapola <boyangbass@gmail.com>
2019-12-18 21:21:45 -0600

    require mail

--- controller/mailcontroller.js
+++ controller/mailcontroller.js
@@ -1,10 +1,12 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 module.exports ={
     mail: function (req,res){
         try{
     
             const {name, email, message} =req.body;
             const msg = {
-                to: 'boyangbass@gmail.com',
+                to: 'palletsound@gmail.com',
                 from: email,
                 subject: `${name} would like to book a session`,
                 text: message,
